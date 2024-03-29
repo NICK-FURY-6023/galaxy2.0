8481e41ae63192bf2be9d093d42a5c5e8b8aacef
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-03-29 12:24:00 +0530

    Update guildDelete.js

--- src/listeners /guild /guildDelete.js
+++ src/listeners /guild /guildDelete.js
@@ -1,2 +1,9 @@
-gger.success(`${ chalk.redBright('[GUILD REMOVE]') } ${ guild.name } has removed the bot!`);
+const logger = require('@mirasaki/logger');
+const chalk = require('chalk');
+
+module.exports = (client, guild) => {
+  // Always check to make sure the guild is available
+  if (!guild?.available) return;
+  // Logging the event to our console
+  logger.success(`${ chalk.redBright('[GUILD REMOVE]') } ${ guild.name } has removed the bot!`);
 };
